b2a93f463eca0a0ce6f80cf4985c069954e5e6e7
amirh-bakhtiari <bakhtiari.amir.h@gmail.com>
2022-05-09 22:27:07 +0430

    Add get_frames function

---
+++ src/VideoUtility.py
@@ -0,0 +1,29 @@
+def get_frames(vid_path: str, vid_pix_fmt: str = "yuv420p", frame_color_mode: str = 'rgb', 
+                   height: int = 432, width: int = 768) -> ndarray:
+    ''' Get an input path containing a video and return its frames
+    
+    :param path_in: path of a video to extract
+    :param vid_pix_fmt: pixel format of a YUV video (not used for mp4 videos)
+    :param frame_color_mode: extracted frames'pixel format ('rgb' or 'gray') (not used for mp4 videos)
+    :param height: height of a YUV video frame (not used for mp4 videos)
+    :param width: width of a YUV video frame (not used for mp4 videos)
+    :return: an array of video frames of the size (num of frames * height * width * num of channels)
+    '''
+    
+    # Get the video extension
+    extension = vid_path.split('.')[-1]
+    # Check the video type to set the proper params
+    if extension == 'mp4':
+        return skvideo.io.vread(vid_path)
+    # Otherwise check the output frame color mode for YUV videos
+    elif frame_color_mode == 'rgb':
+        return skvideo.io.vread(vid_path, height, width, inputdict={"-pix_fmt": "yuv420p"})
+    elif frame_color_mode == 'gray':
+        return skvideo.io.vread(vid_path, height, width, inputdict={"-pix_fmt": "yuv420p"},
+                            outputdict={"-pix_fmt": "gray"})
+    else:
+        return None
+    
+
+
+        
