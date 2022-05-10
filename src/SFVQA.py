b862f4a8b4953fe1554502c6cdfbbf2549267f90
amirh-bakhtiari <bakhtiari.amir.h@gmail.com>
2022-05-10 18:53:42 +0430

    Define get_geatures() in SFVQA

--- src/SFVQA.py
+++ src/SFVQA.py
@@ -16,3 +16,28 @@ def set_sf_model(device: torch.device) -> nn.Module:
     vgg.to(device)
         
     return vgg
+
+def get_features(image, model, layers=None):
+    """ Run an image forward through a model and get the features for 
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    
+    ## TODO: Complete mapping layer names of PyTorch's VGGNet to names from the paper
+    ## Need the layers for the content and style representations of an image
+    if layers is None:
+        layers = {'0': 'conv1_1',
+                  '5': 'conv2_1', 
+                  '10': 'conv3_1', 
+                  '19': 'conv4_1',
+                  '21': 'conv4_2',  ## content representation
+                  '28': 'conv5_1'}
+        
+    features = {}
+    x = image
+    # model._modules is a dictionary holding each module in the model
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+            
+    return features
