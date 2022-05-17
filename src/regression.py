082f6e82c08fc9cce60fe7cbb24b813e98a4c745
amirh-bakhtiari <bakhtiari.amir.h@gmail.com>
2022-05-17 16:47:00 +0430

    Define live_dataset_regression() in regression.py

---
+++ src/regression.py
@@ -0,0 +1,87 @@
+# Enable Intel(R) Extension for Scikit-learn
+from sklearnex import patch_sklearn
+patch_sklearn()
+from sklearn.model_selection import GroupShuffleSplit
+from sklearn.preprocessing import StandardScalar
+from sklearn.svm import SVR
+
+
+def live_dataset_regression(X: ndarray, y: ndarray):
+    '''Train an SVR Using the video level features and their corresponding scores,
+       predict the scores of test videos using the trained SVR. Finally calculate the
+       Spearman and Pearson correlation for target and predicted scores.
+       
+    :param X: video level features of all videos in the dataset
+    :param y: scores of all videos in the dataset
+    '''
+    
+    video_data = '/media/amirh/Programs/Projects/VQA_Datasets/LIVE_SD/live_video_quality_seqs.txt'
+    dmos_data = '/media/amirh/Programs/Projects/VQA_Datasets/LIVE_SD/live_video_quality_data.txt'
+    # Get the list of video sequences
+    video_list, _ = dh.get_live_info(video_data, dmos_data)
+    
+    # Turn y into a 2D array to match StandardScalar() input
+    y = y.reshape(-1, 1)
+    
+    # There are 10 pristine videos in LIVE VQA dataset and 15 different distorted videos are made from each,
+    # totally 150 videos. Divide the video features into 10 groups in orders, so that the same group will not
+    # appear in two different folds
+    groups = np.empty(150, dtype='u1')
+    video_groups = []
+    
+    for i in range(0, 150, 15):
+        groups[i: i + 15] = i / 15
+        # Add the name of the first video in each group
+        video_groups.append(video_list[i])
+               
+    gss = GroupShuffleSplit(n_splits=50, train_size=0.8)
+    
+    SROCC_coef, SROCC_p, PLCC = [], [], []
+    
+    for train_idx, test_idx in gss.split(X, y, groups):
+        # Split train validation set
+        X_train, y_train = X[train_idx], y[train_idx]
+        X_test, y_test = X[test_idx], y[test_idx]
+        
+        # print the video names fallen into test split
+        for group in set(groups[test_idx])
+            print(f'video group {group} = {video_groups[group]}', end=',  ')
+        
+        # Feature scaling
+        sc_X = StandardScalar()
+        sc_y = StandardScalar()
+        
+        X_train = sc_X.fit_transform(X_train)
+        X_test = sc_X.transform(X_test)
+        y_train = sc_y.fit_transform(y_train)
+        
+        # Set the regressor to SVR
+        regressor = SVR(kernel='rbf')
+        
+        # Train the regresoor model
+        regressor.fit(X_train, y_train)
+        
+        # Predict the scores for X_test videos features
+        y_pred = regressor.predict(X_test)
+        
+        # Inverse transform the predicted values to get the real values
+        y_pred = sc_y.inverse_transform(y_pred)
+        
+        # Calculate the Spearman rank-order correlation
+        coef, p = spearmanr(y_test, y_pred)
+        
+        # Calculate the Pearson correlation
+        corr, _ = pearsonr(y_test, y_pred)
+        
+        SROCC_coef.append(coef)
+        SROCC_p.append(p)
+        PLCC.append(corr)
+        
+        print(f'\nSpearman correlation = {coef:.4f} with p = {p:.4f},  Pearson correlation = {corr:.4f}')
+        print('*' * 50)
+        
+        
+        
+        
+    
+    
