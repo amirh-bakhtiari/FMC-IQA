596428ba1e46dcc1b6aa4ac3612233b684886e97
amirh-bakhtiari <bakhtiari.amir.h@gmail.com>
2022-05-17 16:51:23 +0430

    move feature_pooling() to pooling.py and rename to simple_pooling()

---
+++ src/pooling.py
@@ -0,0 +1,19 @@
+def simple_pooling(videos_features: list, pooling: str = 'max') -> ndarray:
+    '''Get a list of videos with features for each frame, pool the features of all frames of 
+       each video using the given method to have a single vector as the final video level features
+       
+    :param videos_features: a list containing frames features of all videos in video dataset
+    :param pooling: specify the pooling method
+    :return: an array of video level features of all videos
+    '''
+    
+    videos_features = np.array(videos_features)
+    
+    if pooling == 'max':
+        video_level_features = np.max(videos_features, axis=1)
+    elif pooling == 'mean':
+        video_level_features = np.mean(video_features, axis=1)
+    elif pooling == 'min':
+        video_level_features = np.min(video_features, axis=1)
+        
+    return video_level_features
